3e5f0c2446b2348bae0c2859905295ff2a07a87c
YA <vector_cp@mail.ru>
2021-05-20 23:24:56 +0300

    refactor: add server.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const db = require('./db');
 const user = require('./controllers/usercontroller');
-const game = require('./controllers/gamecontroller')
+const game = require('./controllers/gamecontroller');
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
 
 db.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
+// app.listen(PORT, ()=> {
+//     console.log("App is listening on 4000");
+// })
+module.exports = app;
